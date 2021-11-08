2a69b201dec01291d34580e68d41e64393793e66
Evan Chisholm <elchisholm248@gmail.com>
2021-11-08 16:04:22 -0500

    finished the modern art section although subtask 4 does not get completed because it is too slow

--- modern-art/main.py
+++ modern-art/main.py
@@ -2,44 +2,32 @@ def createMatrix(width, height):
     matrix = {}
     for x in range(0, width):
         for y in range(0, height):
-            matrix[(x,y)] = 'black'
+            matrix[(x,y)] = False
 
     return matrix
 
 def paintLine(matrix, direction, pos, width, height):
     # if it is a c go down that column and flip each cell from black to gold or gold to black
     # create matrix m
+    realpos = pos - 1
     if direction == 'C':
         for y in range(0, height):
-            if matrix[(pos, y)] == 'black':
-                matrix[(pos, y)] = 'gold'
-            else:
-                matrix[(pos, y)] = 'black'
+            matrix[(realpos, y)] = not matrix[(realpos, y)]
     else:
         for x in range(0, width):
-            if matrix[(x, pos)]== 'black':
-                matrix[(x, pos)] = 'gold'
-            else:
-                matrix[(x, pos)] = 'black'
+            matrix[(x, realpos)] = not matrix[(x, realpos)]
 
 def countGold(matrix, width, height):
     count = 0
     for x in range(0, width):
         for y in range(0, height):
-            if matrix[(x, y)] == 'gold':
+            if matrix[(x, y)]:
                 count += 1
     return count 
 
-def printGrid(matrix, width, height):
-    for y in range(0, height):
-        row = 
-        for
-
-
-
 # read inputs m and n on 2 lines
-m = int(input())
 n = int(input())
+m = int(input())
 
 # create a matrix with a width M and height N
 canvas = createMatrix(m, n)
