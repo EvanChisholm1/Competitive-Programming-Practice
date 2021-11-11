ba5a867b59be0a628e602dd95aac3f7d45233304
Evan Chisholm <elchisholm248@gmail.com>
2021-11-10 21:29:28 -0500

    came up with a way to solve the modern art question that should be faster. works on sample input 1

--- modern-art/main.py
+++ modern-art/main.py
@@ -1,36 +1,48 @@
-def createMatrix(width, height):
-    matrix = {}
-    for x in range(0, width):
-        for y in range(0, height):
-            matrix[(x,y)] = False
+# def createMatrix(width, height):
+#     matrix = {}
+#     for x in range(0, width):
+#         for y in range(0, height):
+#             matrix[(x,y)] = False
 
-    return matrix
+#     return matrix
 
-def paintLine(matrix, direction, pos, width, height):
+def paintLine(rows, cols, direction, pos): 
     # if it is a c go down that column and flip each cell from black to gold or gold to black
     # create matrix m
     realpos = pos - 1
     if direction == 'C':
-        for y in range(0, height):
-            matrix[(realpos, y)] = not matrix[(realpos, y)]
+        cols[realpos] += 1
     else:
-        for x in range(0, width):
-            matrix[(x, realpos)] = not matrix[(x, realpos)]
+        rows[realpos] += 1
 
-def countGold(matrix, width, height):
+    # if direction == 'C':
+    #     for y in range(0, height):
+    #         matrix[(realpos, y)] = not matrix[(realpos, y)]
+    # else:
+    #     for x in range(0, width):
+    #         matrix[(x, realpos)] = not matrix[(x, realpos)]
+
+def countGold(rows, cols):
     count = 0
-    for x in range(0, width):
-        for y in range(0, height):
-            if matrix[(x, y)]:
+    for col in cols:
+        for row in rows:
+            if row + col % 2 == 1:
                 count += 1
+    # for x in range(0, width):
+    #     for y in range(0, height):
+    #         if matrix[(x, y)]:
+    #             count += 1
     return count 
+
 
 # read inputs m and n on 2 lines
 n = int(input())
 m = int(input())
+cols = [0] * m
+rows = [0] * n
 
 # create a matrix with a width M and height N
-canvas = createMatrix(m, n)
+# canvas = createMatrix(m, n)
 
 # read input K 
 k = int(input())
@@ -41,7 +53,7 @@ for i in range(0, k):
     inputStr = input()
     direction = inputStr.split()[0]
     position = int(inputStr.split()[1])
-    paintLine(canvas, direction, position, m, n)
+    paintLine(rows, cols, direction, position)
 
-amountOfGold = countGold(canvas, m, n)
+amountOfGold = countGold(rows, cols)
 print(amountOfGold)
